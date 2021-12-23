302af3ca465cb9640d50c811097cf49f9943ed71
devgis <devgis@163.com>
2021-12-23 09:45:41 +0800

    V0
    .net core 3.1 .net 5 OK

---
+++ src/WebAPIWithSwagger/Startup.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.Swagger;
+
+using Swashbuckle.Application;
+using Swashbuckle.SwaggerUi;
+using Swashbuckle.Swagger.Annotations;
+using Microsoft.OpenApi.Models;
+using System.IO;
+
+namespace WebAPIWithSwagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("sparktodo", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "SparkTodo API",
+                    Description = "API for SparkTodo",
+                    Contact = new OpenApiContact() { Name = "WeihanLi", Email = "weihanli@outlook.com" }
+                });
+
+                // include document file
+                option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{typeof(Startup).Assembly.GetName().Name}.xml"), true);
+            });
+
+            services.AddSwaggerGen(option =>
+            {
+                // ...
+
+                // Add security definitions
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Description = "Please enter into field the word 'Bearer' followed by a space and the JWT value",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference()
+            {
+                Id = "Bearer",
+                Type = ReferenceType.SecurityScheme
+            }
+        }, Array.Empty<string>() }
+    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            //Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint("/swagger/sparktodo/swagger.json", "sparktodo Docs");
+
+                option.RoutePrefix = string.Empty;
+                option.DocumentTitle = "SparkTodo API";
+            });
+        }
+    }
+}
